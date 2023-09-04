a3f07aa1385ede36f3100c03411449caedd82d22
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-04 22:59:06 +0600

    parsing data in api

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
+import { useRef } from "react";
+
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div>
           <label htmlFor="email">Your email</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
 
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea id="feedback" rows="5"></textarea>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
